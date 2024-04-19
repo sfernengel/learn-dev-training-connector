ae88583a13c1a41c670bc19a1188a7d8b7cd6d51
Nagesh Dixit <nagesh.dixit-ext@commercetools.com>
2024-04-19 07:54:38 -0400

    fixed the job to add products

--- manage-new-arrivals-job-app/src/api/products.ts
+++ manage-new-arrivals-job-app/src/api/products.ts
@@ -2,17 +2,16 @@ import { ClientResponse, ProductPagedQueryResponse, ProductProjectionPagedQueryR
 
 import { createApiRoot } from '../client/create.client';
 import { logger } from '../utils/logger.utils';
-import { response } from 'express';
 
 
-export const getProductsInCategory = async(filterQuery:string[]) =>{
+export const getProductsByQuery = async(filterQuery:string[]) =>{
   const {body} = await createApiRoot()
   .productProjections()
   .search()
   .get({
     queryArgs: {
       "filter.query": filterQuery,
-      limit: 500,
+      limit: 100,
     },
   })
   .execute();
@@ -30,63 +29,34 @@ export const removeCategoryFromProduct = async(
     .post({
       body: {
         version: productVersion,
-        actions: [
-          {
-            action: "removeFromCategory",
-            category: {
-              typeId: "category",
-              id: categoryId,
-            },
-            staged:false
+        actions: [{
+          action: "removeFromCategory",
+          category: {
+            typeId: "category",
+            id: categoryId,
           },
-        ],
+          staged:false
+        }],
       },
     })
     .execute();
-    logger.info(`Removing: ${productId}`)
     return body; 
 };
 
-export const getProductById = async(productId: string) => {
-    return await createApiRoot()
-      .productProjections()
-      .withId({ ID: productId })
-      .get()
-      .execute();
-};
-
-export const getProductsPublishedInMinutes = async(minutes: number) => {
-  const tenMinutesAgo = new Date(Date.now() - minutes * 60 * 1000);
-  return await createApiRoot()
-    .productProjections()
-    .get({
-      queryArgs: {
-        staged: false,
-        where: `lastModifiedAt > "${tenMinutesAgo.toISOString}"`,
-        sort: 'lastModifiedAt desc',
-        limit: 100
-      }
-    })
-    .execute().then((response) => response.body.results)
-};
-
-export const addCategoryToProductById = async(productId: string, categoryId: string) => {
-    return getProductById(productId).then(({body}) => {
-        return createApiRoot()
+export const addCategoryToProductById = async(productId: string, productVersion: number, categoryId: string) => {
+    const {body} = await createApiRoot()
             .products()
             .withId({ID: productId})
             .post({
                 body: {
-                    version: body.version,
+                    version: productVersion,
                     actions: [{
-                    action: "addToCategory",
-                    category: {typeId: "category", id: categoryId}
-                    },
-                    {
-                    action: "publish"    
+                      action: "addToCategory",
+                      category: {typeId: "category", id: categoryId},
+                      staged:false
                     }]
                 }
             })
             .execute();
-        });
+    return body; 
 };
--- manage-new-arrivals-job-app/src/controllers/job.controller.ts
+++ manage-new-arrivals-job-app/src/controllers/job.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { readConfiguration } from '../utils/config.utils';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
-import { addCategoryToProductById, getProductsInCategory, getProductsPublishedInMinutes, removeCategoryFromProduct } from '../api/products';
+import { addCategoryToProductById, getProductsByQuery, removeCategoryFromProduct } from '../api/products';
 import { getCategoryByKey } from '../api/categories';
 
 /**
@@ -25,31 +25,39 @@ export const post = async (_request: Request, response: Response) => {
     const toDate = new Date(new Date().setDate(today.getDate() - 30));
 
     // Filter query used by Product Projection Search
-    const filterQuery:string[] = [
+    const oldProductsFilterQuery:string[] = [
       `categories.id:"${categoryId}"`,
       `createdAt:range (* to "${toDate.toISOString()}")`,
     ]
+    
+    const oldProductsInCategory = await getProductsByQuery(oldProductsFilterQuery);
 
-    const productsInCategory = await getProductsInCategory(filterQuery);
-
-    for (const product of productsInCategory.results)
+    for (const product of oldProductsInCategory.results)
     {
       await removeCategoryFromProduct(product.id, product.version, categoryId);
     }
 
     logger.info(`Finished removing Products created before ${toDate.toDateString()} in category: ${categoryId}`);
-        
-    await getProductsPublishedInMinutes(10)
-      .then((products) => 
-        products.map(product => {
-          const createdAt = new Date(product.createdAt);
-          
-          if((createdAt >= toDate) && (product.categories?.find(category => category.id === categoryId) == undefined))
-          {
-            addCategoryToProductById(product.id, categoryId);
-          }
-        })
-      );
+
+    const tenMinutesAgo = new Date(Date.now() - 11 * 60 * 1000);
+    const now = new Date(Date.now());
+    
+    const newProductsFilterQuery:string[] = [
+      `lastModifiedAt:range ("${tenMinutesAgo.toISOString()}" to "${now.toISOString()}")`,
+      `createdAt:range ("${toDate.toISOString()}" to "${now.toISOString()}")`,
+    ]
+    logger.info("Running job to add new Products to the new arrivals Category.");
+    
+    const newProductsNotInCategory = await getProductsByQuery(newProductsFilterQuery);
+
+    if (newProductsNotInCategory.count >0) {
+      for (const product of newProductsNotInCategory.results)
+      {
+        if (product.categories?.find(category => category.id === categoryId) == undefined){
+          await addCategoryToProductById(product.id, product.version, categoryId);
+        }
+      }
+    }
     
     logger.info(`Finished adding new products created in the category: ${categoryId}`);
     
@@ -58,7 +66,7 @@ export const post = async (_request: Request, response: Response) => {
   } catch (error) {
     throw new CustomError(
       500,
-      `Internal Server Error - Error retrieving all orders from the commercetools SDK`
+      `Internal Server Error - Error retrieving all products from the commercetools SDK`
     );
   }
 };
