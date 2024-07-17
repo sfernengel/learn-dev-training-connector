c69eb38c5d66b7ad748e41d129477345e63463d9
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2024-07-17 14:32:49 +0200

    Activate error handling middleware

--- mail-sender-event-app/src/routes/mail-sending.route.ts
+++ mail-sender-event-app/src/routes/mail-sending.route.ts
@@ -3,10 +3,13 @@ import { Router } from 'express';
 import { messageHandler } from '../controllers/mail-sending.controller.js';
 const eventRouter = Router();
 
-eventRouter.post('/', async (req, res) => {
-  await messageHandler(req, res);
-  res.status(200);
-  res.send();
+eventRouter.post('/', async (req, res, next) => {
+  console.log('A request has been received');
+  try {
+    await messageHandler(req, res);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default eventRouter;
